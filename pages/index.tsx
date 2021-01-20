aea3ef0065e1bbd9720dab9b1300f0922acbe654
Nikolay Galkin <mr.galkin@gmail.com>
2021-01-20 20:29:04 +0200

    Tilt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import React from 'react'
 import Head from 'next/head'
 import { NextPage } from 'next'
-import Header from '../components/header'
-import Footer from '../components/footer'
+// import Tilt from 'react-vanilla-tilt'
+import Tilt from 'react-tilt'
 
 const HomePage: NextPage = () => (
   <>
@@ -10,7 +11,12 @@ const HomePage: NextPage = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <main>
-      <div className="container mx-auto text-center max-width-lg">
+      <Tilt
+        options={{ max: 15, speed: 400, glare: true, 'max-glare': 1 }}
+        className="container z-50 mx-auto text-center max-width-lg"
+        style={{ width: 'auto', height: 'auto', margin: 0, transform: 'none', boxShadow: 'none' }}
+      >
+        {/* <div className="container mx-auto text-center max-width-lg"> */}
         <form className="calculator">
           <input type="text" className="value" defaultValue="55+73" readOnly />
           <span className="num clear">c</span>
@@ -32,7 +38,8 @@ const HomePage: NextPage = () => (
           <span className="num">.</span>
           <span className="num equal">=</span>
         </form>
-      </div>
+        {/* </div> */}
+      </Tilt>
     </main>
   </>
 )

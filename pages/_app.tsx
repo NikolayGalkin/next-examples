68e6f7abc94213536df62079936ca75095a19437
Nikolay Galkin <mr.galkin@gmail.com>
2021-04-12 16:04:36 +0300

    Init StoryBook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@fontsource/poppins'
-import '../styles/globals.css'
-
 import { AppProps } from 'next/app'
+
+import '@fontsource/poppins'
+import 'tailwindcss/tailwind.css'
 
 const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
